60e028090d6580910f1e2c23e27d29a291b11def
Noku159 <atip.pars@bumail.net>
2024-10-02 17:15:04 +0700

    งาน

---
+++ Assets/Scripts/Lab/Character.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+   [SerializeField] private int health;
+   public int Health
+    {
+        get 
+        {
+            return Health;
+        }
+        set
+        {
+            health = value;
+        }
+        
+    }
+
+    public Animator anim;
+    public Rigidbody2D rb;
+
+    public bool Isdead()
+    { 
+        return Health <= 0;
+
+    }
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+    }
+}
